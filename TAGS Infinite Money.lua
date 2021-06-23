a232eebce2b7b8183ba93ddf74c94bafc83f158e
30shots <82278400+30shots@users.noreply.github.com>
2021-06-22 17:24:22 -0700

    Update TAGS Infinite Money.lua

--- TAGS Infinite Money.lua
+++ TAGS Infinite Money.lua
@@ -1,6 +1,6 @@
 -- will be patched when the dev changes/disables the code
 game:GetService("RunService").RenderStepped:Connect(function()
-    local amount = 999999;
+    local amount = 9999999999999;
     local code = "Code10";
     local remote = game:GetService("ReplicatedStorage").EnterCode;
     remote:FireServer(amount, code);
