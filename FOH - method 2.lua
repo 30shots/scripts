c6f4a2e2b786deb37e5c64e4110af6bb61f6a20c
30shots <82278400+30shots@users.noreply.github.com>
2021-06-26 11:16:51 -0700

    Create FOH - method 2.lua

---
+++ FOH - method 2.lua
@@ -0,0 +1,25 @@
+local ws = game:GetService("Workspace")
+local rs = game:GetService("RunService")
+local content = ws.Map.Content
+
+local function destroyDamage()
+    if content then
+        for i1,v1 in pairs(content:GetChildren()) do
+            for i2,v2 in pairs(v1:GetChildren()) do
+                if tostring(v2.Name) == tostring("Damage") then
+                    v2:Destroy()
+                end
+            end
+        end
+    else
+        local msg = Instance.new("Hint")
+        msg.Parent = ws
+        msg.Text = "nothing to delete was found"
+        wait(1)
+        msg:Destroy()
+    end
+end
+
+rs.Stepped:connect(function()
+    destroyDamage()
+end)
